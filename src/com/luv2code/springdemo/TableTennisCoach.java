693aa41c8be8b136554fc5a6e884ae2465e9be30
Bo Li <bl@broadpeakpartners.com>
2019-08-12 23:11:41 -0400

    08/12/2019

--- src/com/luv2code/springdemo/TableTennisCoach.java
+++ src/com/luv2code/springdemo/TableTennisCoach.java
@@ -11,4 +11,10 @@ public class TableTennisCoach implements Coach {
 		return "Practice backhand ball";
 	}
 
+	@Override
+	public String getDailyFortune() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -1,14 +1,38 @@
 package com.luv2code.springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
+	
+	@Autowired
+	@Qualifier("sadFortuneService")
+	private FortuneService fortuneService;
+	
+//	@Autowired
+//	public TennisCoach (FortuneService theFortuneService) {
+//		fortuneService = theFortuneService;
+//	}
+	
+	public TennisCoach() {
+	}
 
 	@Override
 	public String getDailyWorkout() {
-		// TODO Auto-generated method stub
 		return "Practice your backhand volley";
 	}
+
+//	@Autowired
+//	public void setFortuneService(FortuneService fortuneService) {
+//		this.fortuneService = fortuneService;
+//	}
+
+	@Override
+	public String getDailyFortune() {
+		return fortuneService.getFortune();
+	}
+	
 
 }
